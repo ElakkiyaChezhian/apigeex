a7d7c8741e4da60f3bf05536be544f1bcf432135
ElakkiyaChezhian <87109565+ElakkiyaChezhian@users.noreply.github.com>
2022-07-27 14:28:51 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,33 @@
 variable "project_id" {
-    type=string
+    type= string
+}
+variable "project_id2" {
+    type = string
+}
+variable "network" {
+  type = string
+}
+variable "google_compute_global_address" {
+    type = string
+}
+variable "region" {
+    type = string
+}
+variable "google_apigee_environment" {
+    type = string
+}
+variable "google_apigee_envgroup" {
+    type = string
+}
+variable "google_apigee_instance" {
+    type = string
+}
+variable "google_compute_region_backend_service" {
+    type = string
+}
+variable "google_compute_health_check" {
+    type = string
+}
+variable "google_compute_forwarding_rule" {
+    type = string
 }
